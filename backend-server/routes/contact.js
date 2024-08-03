d5fed04d8289b100f5066548cfbf8c7c23f40f6c
scrsund <137787953+scrsund@users.noreply.github.com>
2024-08-03 23:05:48 +0200

    fixing env

--- backend-server/routes/contact.js
+++ backend-server/routes/contact.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 console.log("Supabase URL:", supabaseUrl);
 console.log("Supabase Key:", supabaseKey ? "Key is set" : "Key is missing");
