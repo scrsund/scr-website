958fdd16932c9d5d7f9c39f1a9bc9ad5adcea748
scrsund <137787953+scrsund@users.noreply.github.com>
2024-09-19 12:28:13 +0200

    Save local changes before pulling remote main

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const store = createStore({
         {
           title: "Project 3",
           icon: "fa-solid fa-code",
-          description: "",
+          description: "<i>• Vuex<br>• Vue Router<br>• PostgreSQL<br>• Express.js",
           heading: "Trading Journal<br><i>(coming soon)</i>",
           customBorderClass: "border-bottom-left",
         },
@@ -109,6 +109,52 @@ const store = createStore({
       //   description:
       //     "Helping others and being generous when I can be. I've been volunteering my time at various organizations my whole life.",
       //   heading: "On-Going",
+      //   customBorderClass: "border-top-left",
+      // },
+      // {
+      //   title: "Self-Taught",
+      //   icon: "fa-solid fa-graduation-cap",
+      //   description:
+      //     "I taught myself how to do music, how to understand financial markets, and now, how to code. Learning new skills and challenging myself is my hobby.",
+      //   heading: "2007 - Present",
+      //   customBorderClass: "border-top-bottom",
+      // },
+      // {
+      //   title: "Me",
+      //   icon: null,
+      //   description: null,
+      //   customBorderClass: "border-none",
+      // },
+      // {
+      //   title: "Leader",
+      //   icon: "fa-solid fa-trophy",
+      //   description:
+      //     "Led as a music instructor, youth leader, and preschool assistant. I've worked with people of all ages-- from 2 years to 60 years old.",
+      //   heading: "2016 - 2022",
+      //   customBorderClass: "border-top-bottom",
+      // },
+      // {
+      //   title: "Aspiring Developer",
+      //   icon: "fa-solid fa-code",
+      //   description:
+      //     "Python, JavaScript/HTML/CSS and postgreSQL. Although I'm open to both front and backend development, I've been enjoying frontend development recently.",
+      //   heading: "2023 - Present",
+      //   customBorderClass: "border-top-right",
+      // },
+      // {
+      //   title: "Financial Analyst",
+      //   icon: "fa-solid fa-coins",
+      //   description:
+      //     "Trading Forex, Cryptocurrency, and NFTs through market sentiment and technical analysis. I've been able to multiply my portfolio in the last 3 years.",
+      //   heading: "2021 - Present",
+      //   customBorderClass: "border-left-right",
+      // },
+      // {
+      //   title: "Musician",
+      //   icon: "fa-solid fa-music",
+      //   description:
+      //     "Playing the drums, guitar, piano, bass, and writing songs. I've played for crowds of thousands and have experience as a studio musician.",
+      //   heading: "2008 - 2019",
       //   customBorderClass: "border-top-left",
       // },
     };
